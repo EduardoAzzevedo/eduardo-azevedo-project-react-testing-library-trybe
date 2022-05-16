995a06275dfc6e84aba2921959cf3f3caf95bea6
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-05-16 20:32:59 -0300

    Requisito 4 NotFound.js.

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,20 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import renderWithRouter from '../renderWithRouter';
+import NotFound from '../components/NotFound';
+
+describe('Testa o componente NotFound', () => {
+  it('Testa se contém mensagem de erro', () => {
+    renderWithRouter(<NotFound />);
+    const title = screen.getByRole('heading',
+      { level: 2, name: /page requested not found/i });
+    expect(title).toBeInTheDocument();
+  });
+
+  it('Testa se contém o Pikachu chorando', () => {
+    renderWithRouter(<NotFound />);
+    const imagem = screen.getByRole('img', { name: /pikachu crying/i });
+    expect(imagem).toBeInTheDocument();
+    expect(imagem).toHaveAttribute('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
