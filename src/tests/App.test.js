a3edd11420e223a27be3625aed1f10e9e1e871d3
Eduardo Azevedo <eduardo-azevedo@outlook.com>
2022-05-16 17:37:40 -0300

    Requisito 1 App.js, adiciona render e testes.

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,45 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../renderWithRouter';
+import App from '../App';
+
+describe('Requisito 1 - Teste o componente App', () => {
+  it('Testa o conjunto de links de navegação na tela', () => {
+    renderWithRouter(<App />);
+    const linkParaHome = screen.getByText('Home');
+    const linkParaAbout = screen.getByRole('link', { nome: /about/i });
+    const linkParaFavorites = screen.getByRole('link', { nome: /favorite pokémons/i });
+
+    expect(linkParaHome).toBeInTheDocument();
+    expect(linkParaAbout).toBeInTheDocument();
+    expect(linkParaFavorites).toBeInTheDocument();
+  });
+
+  it('Testa se é redirecionado para a pagina inicial ao clicar no link HOME', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkParaHome = screen.getByText('Home');
+    userEvent.click(linkParaHome);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  it('Testa se é redirecionado para pagina Favorite Pokemons', () => {
+    const { history } = renderWithRouter(<App />);
+    const linkParaFavorites = screen.getByRole('link', { nome: /favorite pokémons/i });
+    userEvent.click(linkParaFavorites);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  it('Testa se a pagina Not Found é encontrada quando entrar em outra URl ', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/pagina-nao-existe');
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/pagina-nao-existe');
+    expect(screen.getByText(/Page requested not found/i)).toBeInTheDocument();
+  });
+});
